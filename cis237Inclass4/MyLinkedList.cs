f2d20f78fd92ddbd10fe90f4a11230388b382c4e
David Barnes <barnesdavidj@gmail.com>
2015-10-15 16:45:48 -0400

    Add the add method and delete method

---
+++ cis237Inclass4/MyLinkedList.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237Inclass4
+{
+    class MyLinkedList
+    {
+        //Private variable to hold the current position of where we are in the linked list
+        private Node current;
+
+        //A public property that will point to the Head node. (The first one in the list)
+        public Node Head
+        {
+            get;
+            set;
+        }
+
+        //Constructor. It will set the Head to null because there is nothing in the list yet.
+        public MyLinkedList()
+        {
+            Head = null;
+        }
+
+        public void Add(string content)
+        {
+            //Make a new node to add to the linked list
+            Node node = new Node();
+            //Set the data to the passed in content
+            node.Data = content;
+
+            //This will add the first element to our list
+            if(Head == null)
+            {
+                Head = node;
+            }
+            //Not the first node, so set the new node to the current node's next variable
+            else
+            {
+                current.Next = node;
+            }
+            //Move down the list. Set current to the new node we added.
+            current = node;
+        }
+
+        public bool Delete(int Position)
+        {
+            //Set current to Head. Need to walk through it from the beginning
+            current = Head;
+
+            //If the position is the very first node in the list
+            if (Position == 1)
+            {
+                //Set the Head to the next node in the list
+                Head = current.Next;
+                //Delete the current.Next pointer so there is no reference from current to
+                //another node
+                current.Next = null;
+                //current = null because we want the garbage collector to come pick it up.
+                current = null;
+                //it was successfull so, return true;
+                return true;
+            }
+            //Check to make sure that at least a positive number was entered.
+            //Should also check to make sure that the position is less than the
+            //size of the array so that we aren't looking for something that doesn't
+            //exist. Adding a size property will be more work.
+            //TODO: Add a size property
+            if (Position > 1)
+            {
+                //Make a temp node that starts at the Head. This way we don't need to actually
+                //move the Head pointer. We can just use the temp node
+                Node tempNode = Head;
+                //Set a last node to null. It will be used for the delete
+                Node lastNode = null;
+                //Start a counter to know if we have reached the position yet or not.
+                int count = 0;
+
+                //while the tempNode is NOT null, we can continue to walk through the
+                //linked list. If it is null, then we have reached the end.
+                while (tempNode != null)
+                {
+                    //If the count is the same as the position entered - 1, then we have found
+                    //the one we would like to delete.
+                    if (count == Position - 1)
+                    {
+                        //Set the last node's Next proplerty to the TempNodes Next Property
+                        lastNode.Next = tempNode.Next;
+                        //Remove the next pointer of the tempnode
+                        tempNode.Next = null;
+                        //Return True because it was successfull
+                        return true;
+                    }
+                    //Increment the counter since we are going to move forward in the list
+                    count ++;
+                    //Set the lastNode equal to the tempNode. Now both variables are pointing to
+                    //the exact same node.
+                    lastNode = tempNode;
+                    //Now set the tempNode to tempNodes Next node. This will move tempNode
+                    //one more location forward in the list.
+                    tempNode = tempNode.Next;
+                }
+            }
+            //tempNode became null, so apparently we did not find it. Return false.
+            return false;
+        }
+
+
+
+
+    }
+}
--- cis237Inclass4/Program.cs
+++ cis237Inclass4/Program.cs
@@ -10,6 +10,38 @@ namespace cis237Inclass4
     {
         static void Main(string[] args)
         {
+            //Define a new linked list to use
+            MyLinkedList myLinkedList = new MyLinkedList();
+
+            //Add a bunch of stuff to it
+            myLinkedList.Add("first");
+            myLinkedList.Add("second");
+            myLinkedList.Add("third");
+            myLinkedList.Add("fourth");
+
+            //Loop through with this differently looking for loop to output
+            //In here, the first part is initalization: Setting x to the Head
+            //the second part is the test: If x != null, keep going
+            //The last part is: Set the current x to x's next porinter. (The next in the list)
+            for (Node x = myLinkedList.Head; x != null; x = x.Next)
+            {
+                Console.WriteLine(x.Data);
+            }
+
+            //Couple of blank lines
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Delete the 2nd element in the list
+            myLinkedList.Delete(2);
+            //Delete the new 2nd element in the list. Was 3rd before previous delete
+            myLinkedList.Delete(2);
+
+            for (Node x = myLinkedList.Head; x != null; x = x.Next)
+            {
+                Console.WriteLine(x.Data);
+            }
+
         }
     }
 }
